c4d88a4d078528b18409f74ccc8387719d789538
shenyue <shenyue@tudou.com>
2016-02-14 10:37:31 +0800

    base view controller

---
+++ MicroBang/Base/MBBaseNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  MBBaseNavigationController.m
+//  MicroBang
+//
+//  Created by leon on 2016/二月/14.
+//  Copyright © 2016年 leon. All rights reserved.
+//
+
+#import "MBBaseNavigationController.h"
+
+@interface MBBaseNavigationController ()
+
+@end
+
+@implementation MBBaseNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
